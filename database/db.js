68e076663436f0c8bdee3fa50ab364f396ed67fc
laynefaler <laynefaler@gmail.com>
2016-11-28 21:11:24 -0800

    No user db

--- database/db.js
+++ database/db.js
@@ -1,10 +1,6 @@
 const Sequelize = require('Sequelize')
 const db = new Sequelize('movMe', 'root', '')
 
-const User = db.define('user', {
-    name: Sequelize.STRING(20)
-})
-
 const Fav = db.define('favorite', {
     image: Sequelize.STRING(50),
     title: Sequelize.STRING(50),
@@ -12,11 +8,6 @@ const Fav = db.define('favorite', {
     rating: Sequelize.NUMEBR()
 })
 
-User.hasMany(Fav)
-Fav.belongsTo(User)
-
-User.sync()
 Fav.sync()
 
-module.exports = User
 module.exports = Fav
