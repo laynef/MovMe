1af6ced430834020c26c583426b1dc766e37250b
laynefaler <laynefaler@gmail.com>
2016-11-29 16:56:53 -0800

    Minor changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,7 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-<<<<<<< HEAD
     entry: "./app/Root.jsx",
-=======
-   entry: "./app/Root.jsx",
->>>>>>> workingBranch
    output: {
         filename: "public/bundle.js"
     },
@@ -29,16 +25,3 @@ module.exports = {
         new ExtractTextPlugin('public/main.css', { allChunks: true })
     ]
 }
-<<<<<<< HEAD
-=======
-
-/* 
-gem install sass
-gem install bourbon
-
-npm install --save-dev babel-core babel-loader babel-preset-es2015 babel-preset-react webpack webpack-dev-server node-sass sass-loader extract-text-webpack-plugin css-loader style-loader
-
-npm install --save  sequelize pg pg-hstore mysql sqlite3 tedious axios react react-dom react-router express body-parser morgan 
-*/
-
->>>>>>> workingBranch
