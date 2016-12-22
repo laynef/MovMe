e4af3eb9a6c3ae1c9e8bad594efec423825176ea
laynefaler <laynefaler@gmail.com>
2016-12-22 15:21:14 -0800

    Fix double click

--- app/component/Favorites.jsx
+++ app/component/Favorites.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import axios from 'axios'
 
+import { Button, Carousel, CarouselCaption, CarouselItem, Grid, Col, Row, Thumbnail } from 'react-bootstrap'
+
 export default class Favorites extends React.Component {
 
     constructor(props) {
@@ -45,24 +47,52 @@ export default class Favorites extends React.Component {
     }
 
     displayFavorites() {
-        return this.state.list.map((e,i) => (
-            <Card style={gambler}>
-                <CardMedia overlay={<CardTitle title={e.title} />} >
-                <img src={'https://image.tmdb.org/t/p/w500' + e.poster_path} />
-                </CardMedia>
-                <CardText>
-                    <p className="movieRating">{'Rating: ' + e.vote_average}</p> 
-                    <p className="movieSummary">{e.overview}</p>
-                </CardText>
-                <RaisedButton label="Delete Favorite" onClick={this.deleteFavorite.bind(this, [i])} />
-            </Card>
+        return this.state.list.map((ele,i) => (
+            <Col xs={6} md={3}>
+                <Thumbnail src={'https://image.tmdb.org/t/p/w500' + ele.poster_path} alt="242x200">
+                    <h3>{ele.title}</h3>
+                    <h5>Rating: {ele.vote_average}</h5>
+                    <p>{ele.overview}</p>
+                    <p>
+                        <Button bsStyle="default" onClick={this.postMovieInfo.bind(this)}>Favorite</Button>
+                    </p>
+                </Thumbnail>
+            </Col>
         ))
     }
 
     render() {
         return (
             <div id="favoritesPage">
-                {this.displayFavorites()}
+                    <Carousel>
+                        <Carousel.Item>
+                        <img width={900} height={500} alt="900x500" src="http://i.imgur.com/ncF41VA.jpg"/>
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                        <img width={900} height={500} alt="900x500" src="http://i.imgur.com/DRNFL2C.jpg"/>
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                        <img width={900} height={500} alt="900x500" src="http://i.imgur.com/bAsrJzX.jpg"/>
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+
+                <Grid>
+                    <Row>
+                        {this.displayFavorites()}
+                    </Row>
+                </Grid>
             </div>
             )
         }
--- app/component/Selector.jsx
+++ app/component/Selector.jsx
@@ -1,44 +1,92 @@
 import React from 'react'
 import axios from 'axios'
+import _ from 'lodash'
 
-import { Button, Carousel, CarouselCaption, CarouselItem } from 'react-bootstrap'
+import { Button, Carousel, CarouselCaption, CarouselItem, Grid, Col, Row, Thumbnail } from 'react-bootstrap'
 
 // const options = ["pop", "newReleases", "topRated", "comedies", "action", "adventure", "fantasy", "drama", "horror"]
-const options = ["newReleases", "topRated", "pop"]
+const options = ["comedies", "action", "adventure", "fantasy", "drama", "horror"]
+const arr = [
+            "adventureList",
+            "actionList",
+            "horrorList",
+            "comediesList",
+            "dramaList",
+            "fantasyList"
+            ]
+
+const imgs = [
+"http://i.imgur.com/ncF41VA.jpg",
+"http://i.imgur.com/DRNFL2C.jpg",
+"http://i.imgur.com/bAsrJzX.jpg"
+]
 
 export default class Selector extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            popList: [],
-            adventureList: [],
-            actionList: [],
-            horrorList: [],
-            comediesList: [],
-            dramaList: [],
-            topRatedList: [],
-            newReleasesList: [],
-            fantasyList: [],
-            currMovie: {}
+            list: {
+                adventureList: [],
+                actionList: [],
+                horrorList: [],
+                comediesList: [],
+                dramaList: [],
+                fantasyList: []
+            },
+            currMovie: {},
+            index : {
+                comediesIndex: 0,
+                fantasyIndex: 0,
+                dramaIndex: 0,
+                horrorIndex: 0,
+                actionIndex: 0,
+                adventureIndex: 0
+            },
+            direction : {
+                comediesDirection: null,
+                fantasyDirection: null,
+                dramaDirection: null,
+                horrorDirection: null,
+                actionDirection: null,
+                adventureDirection: null
+            },
+            funcs : {
+                comediesHandleSelector: () => {},
+                fantasyHandleSelector: () => {},
+                dramaHandleSelector: () => {},
+                horrorHandleSelector: () => {},
+                actionHandleSelector: () => {},
+                adventureHandleSelector: () => {}
+            },
+            curr : ''
         }
     }
 
     componentWillMount() {
+        options.forEach(ele => {
+            let name = ele + 'HandleSelector'
+            let n = {}
+            n[name] = (selectedIndex, e) => {
+                this.state[ele + 'Index'] = selectedIndex    
+                this.state[ele + 'Direction'] = e.direction
+                this.setState(this.state[ele + 'Index']);
+                this.setState(this.state[ele + 'Direction']);
+            }
+            this.state.funcs[name] = n[name].bind(this)
+            this.setState(this.state.funcs)
+        })
         options.forEach(e => {
             this.getMovieInfo(e)
         })
     }
 
-    componentDidMount() {
-        this.gridPicker()
-    }
-
     getMovieInfo(ep) {
-        let listings = ep + 'List'
+        let name = ep + 'List'
         axios.get('/api/' + ep)
             .then((resp) => {
-                this.setState({ listings: resp.data })
+                this.state.list[name] = resp.data
+                this.setState(this.state.list)
             })
             .catch((err) => {
                 console.log(`getMovieInfo error: ${err}`)
@@ -48,6 +96,19 @@ export default class Selector extends React.Component {
     getCurrMovie(e) {
         this.setState({ currMovie: e.target.value })
         this.postMovieInfo()
+    }
+
+    getName(e) {
+        this.setState({curr : e})
+    }
+
+    getAction(selectedIndex, e) {
+        let idx = this.state.curr + 'Index'
+        let dir = this.state.curr + 'Direction'
+        this.state.index[idx] = selectedIndex
+        this.state.direction[dir] = e.direction
+        this.setState(this.state.index)
+        this.setState(this.state.direction)
     }
 
     postMovieInfo() {
@@ -69,55 +130,49 @@ export default class Selector extends React.Component {
         this.postMovieInfo()
     }
 
-    gridPicker() {
-        let arr = options.map(e => {
-        let listing = e + "List"
-        return (
-            <Carousel>
-                {this.state.listing.map(ele => (
-                    <Carousel.Item>
-                        <img width={250} height={250} alt="250x250" src={'https://image.tmdb.org/t/p/w500' + ele.poster_path}/>
-                        <Carousel.Caption>
-                            <h3>{ele.title}</h3>
-                            <Button bsStyle="primary" value={ele} onClick={this.getCurrMovie.bind(this)}>Favorites</Button>
-                            <p>{ele.overview}</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                ))}
-            </Carousel>
-            )
-        })
-    }
-
     render() {
         return (
             <div id="selectorPage">
+
                 {/* Image Div */}
                   <Carousel>
-                    <Carousel.Item>
-                    <img width={900} height={500} alt="900x500" src="http://i.imgur.com/ncF41VA.jpg"/>
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                    <img width={900} height={500} alt="900x500" src="http://i.imgur.com/DRNFL2C.jpg"/>
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                    <img width={900} height={500} alt="900x500" src="http://i.imgur.com/bAsrJzX.jpg"/>
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                    </Carousel.Item>
+                    {imgs.map(e =>
+                        <Carousel.Item>
+                            <img width={900} height={500} alt="900x500" src={e}/>
+                            <Carousel.Caption>
+                                <h3>First slide label</h3>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    )}
                 </Carousel>
+
+                {console.log(this.state.funcs.actionHandleSelector)}
                 {/* Grid Picker */}
-                {this.gridPicker()}
+                  <Grid fluid={true} bsClass="gridMain">
+                  {options.map(name => (
+                    <Carousel wrap={false} activeIndex={this.state.index[name + 'Index']} direction={this.state.direction[name + 'Direction']} onClick={this.getName.bind(this, [name])} onSelect={this.getAction.bind(this)}>
+                        {
+                            _.chunk(this.state[name + 'List'], 4).map((e) => (
+                                <Carousel.Item>
+                                        {e.map(ele => (
+                                            <Col xs={6} md={3}>
+                                                    <Thumbnail src={'https://image.tmdb.org/t/p/w500' + ele.poster_path} alt="242x200">
+                                                        <h3>{ele.title}</h3>
+                                                        <h5>Rating: {ele.vote_average}</h5>
+                                                        <p>{ele.overview}</p>
+                                                        <p>
+                                                            <Button bsStyle="default" value={ele} onClick={this.getCurrMovie.bind(this)}>Favorite</Button>
+                                                        </p>
+                                                    </Thumbnail>
+                                                </Col>
+                                        ))}                                        
+                                    </Carousel.Item>
+                                ))
+                        }
+                        </Carousel>
+                    ))}
+                    </Grid>
             </div>
             )
         }
